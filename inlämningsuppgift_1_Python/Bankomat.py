0dd48c61e9b74d4ab6f86203ce9a6e6ba5bac8e0
Doug André <d.andre070@gmail.com>
2019-09-18 21:44:57 +0200

    Final final

--- inlämningsuppgift_1_Python/Bankomat.py
+++ inlämningsuppgift_1_Python/Bankomat.py
@@ -4,7 +4,7 @@ def errorMessage():
      print("\n\t----------------------------------------") 
      print("\t|(((( Please just enter numbers!!! ))))|")
      print("\t----------------------------------------")
-def buyBuy():
+def byeBye():
     print("\n\n\n\n\t**********************************************")
     print("\t*** Thank you for your using our services! ***")
     print("\t**********************************************\n\n\n\n\n")  
@@ -20,58 +20,58 @@ def EnterAccountNum():
                 print("\n\n\t-->Account already exist<--")    
         except:
             errorMessage()
-def AdmAnAccount(menuAdm1):  
+def LoginMenu(loginToAccount):  
     while True:
         try:              
             print("\n\t1. Withdraw money>")
             print("\t2. Insert money>")
             print("\t3. Show balance>")
             print("\t4. Exit ")
-            menuAdm2 = int(input("\n\tSelect menu options> "))                           
-            if menuAdm2 == 1:  
+            SelectInLoginMenu = int(input("\n\tSelect menu options> "))                           
+            if SelectInLoginMenu == 1:  
                 os.system('cls')
                 menuWithdraw = int(input("\n\n\tEnter amount to withdraw> "))
-                if menuWithdraw <= BankAccounts[menuAdm1] and menuWithdraw > 0:
-                    BankAccounts[menuAdm1] -= menuWithdraw
+                if menuWithdraw <= BankAccounts[loginToAccount] and menuWithdraw > 0:
+                    BankAccounts[loginToAccount] -= menuWithdraw
                     print("\t-------")
                 else:
-                     print(f"\n\tInvalid amount your balance is only --> {BankAccounts[menuAdm1]} €" )                  
-            elif menuAdm2 == 2:
+                     print(f"\n\tInvalid amount your balance is only --> {BankAccounts[loginToAccount]} €" )                  
+            elif SelectInLoginMenu == 2:
                  os.system('cls')
                  menuInsert = int(input("\n\n\tEnter amount to insert> "))
                  if menuInsert < 0:
                     print(f"\n\tTo low cant accept amount below zero! " )  
                  else:                      
-                     BankAccounts[menuAdm1] += menuInsert
+                     BankAccounts[loginToAccount] += menuInsert
                      print("\t+++++++") 
-            elif menuAdm2 == 3:
-                os.system('cls')
-                print(f"\n\n\tYour balance in current account number {menuAdm1} is {BankAccounts[menuAdm1]} €" ) 
-                print("\t---------------------------------------------------",)
-            elif menuAdm2 == 4:
-                os.system('cls')
-                break                          
+            elif SelectInLoginMenu == 3:
+                 os.system('cls')
+                 print(f"\n\n\tYour balance in current account number {loginToAccount} is {BankAccounts[loginToAccount]} €" ) 
+                 print("\t---------------------------------------------------",)
+            elif SelectInLoginMenu == 4:
+                 os.system('cls')
+                 break                          
         except:
             errorMessage()              
 while True:
     try:
         print("\n\n\t**********MAIN MENU*********")
         print("\t* 1. Create an account     *")
-        print("\t* 2. Administer an account *")
+        print("\t* 2. Login to account      *")
         print("\t* 3. Exit                  *")
         print("\t****************************")
-        menuMain = int(input("\tSelect menu options> ")) 
+        SelectInMainMenu = int(input("\tSelect menu options> ")) 
         os.system('cls')
-        if menuMain == 1:
+        if SelectInMainMenu == 1:
             EnterAccountNum()            
-        elif menuMain == 2:
-             menuAdm1 = int(input("\n\n\tEnter existing account number> "))
-             if menuAdm1 in BankAccounts:              
-                AdmAnAccount(menuAdm1)
+        elif SelectInMainMenu == 2:
+             loginToAccount = int(input("\n\n\tEnter existing account number> "))
+             if loginToAccount in BankAccounts:              
+                LoginMenu(loginToAccount)
              else:
                 print("\n\tAccount does not exist!")
-        elif menuMain == 3:
-            buyBuy()
+        elif SelectInMainMenu == 3:
+            byeBye()
             break                            
     except:
         errorMessage()
